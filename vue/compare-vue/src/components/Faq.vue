03278b8a47f06c45c033f464004bd4f15e671da1
Geraldo B. Landre <geraldo.landre@gmail.com>
2017-10-07 20:50:11 -0700

    Vue-based simple web page.
    Signed-off-by: Geraldo B. Landre <geraldo.landre@gmail.com>

---
+++ vue/compare-vue/src/components/Faq.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="faq">
+    <div class="container">
+      <section class="section">
+        <h1 class="title">FAQ</h1>
+        <h2 class="subtitle is-4">Lorem ipsum and all of that jazz.</h2>
+
+        <div class="columns">
+          <div class="column is-one-third">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">My title</p>
+                <p class="answer">My answer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'faq'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import "../mq"
+</style>
